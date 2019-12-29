f766f489685fca91c19c124b8a852cf2c3906390
Jules Lee <jackconsolidated@gmail.com>
2019-12-30 02:57:25 +0800

    Initial Commit

---
+++ TemperatureChallengeSwiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TemperatureChallengeSwiftui
+//
+//  Created by Jules Lee on 12/30/19.
+//  Copyright © 2019 Jules Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
